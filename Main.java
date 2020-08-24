1b7e76209e72c76e987c7410894875b7e5e16c07
cokrehn1 <69870558+cokrehn1@users.noreply.github.com>
2020-08-24 14:35:12 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+class Main {
+  public static void main(String[] args) 
+  {
+  double numbEuro = 0.85;
+  double numbUSD = 10;
+  
+  System.out.println(numbEuro * numbUSD + " Euros ready to be exchanged from " + numbUSD);
+  }
+}
